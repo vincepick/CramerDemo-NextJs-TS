509017b04cb999d47d480b7cee505d7e043e8694
vincepick <vap2@st-andrews.ac.uk>
2024-10-07 00:19:58 +0100

    updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
     <main>
       <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
       <Link href="/users">Users</Link>
-      <ProductCard />
+      {/* <ProductCard /> */}
       <UserDelete />
       <UserCreate />
     </main>
